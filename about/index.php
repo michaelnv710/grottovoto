600a59d8f667e442dff666766c421a71c311c135
michaelnv710 <home@Michaels-MacBook-Pro.local>
2015-12-18 09:47:09 -0600

    more work it's coming together

--- about/index.php
+++ about/index.php
@@ -8,13 +8,14 @@
     <div class="jumbotron">
       <div class="container">
         <h1 id="tagline">About</h1>
-        <p id="subtagline">on the subject of; concerning</p>
+        <p id="subtagline">on the subject of, concerning</p>
       </div>
     </div> 
     <div class="container">
+      <p>I probably wouldn't have made this site if there was already something like it out there already. Action Squad was and still is excellent to read, but nobody ever really improved upon it. It seemed like every other exploring site I visited (mind you only of the Twin Cities based groups) had an element of haphazardness to it. The content wasn't bad, but I felt like every one was just documented their exploration instead of going in with the intention of documenting it as well as possible.[It also seemed like every single place had at least one place that said "more coming soon", it was just not professional.] There were only action crews, no camera crews. Now, the action's still got to be there, but we're going to get a good shot first.</p>
       <h2>Inspiration</h2>
-      <p>For me, this all started when a friend of mine mentioned "The Labyrinth" of utility tunnels underneath downtown St. Paul. Naturally, the conversation turned to Action Squad and the exploration they did in the late 90's and early 00's. I was lead to their <a href="http://www.actionsquad.org">website</a></p>
-      <h2>Disclaimer</h2>
+      <p>For me, this all started when a friend of mine mentioned "The Labyrinth" of utility tunnels underneath downtown St. Paul. Naturally, the conversation turned to Action Squad and the exploration they had done in the late 90's and early 00's. I was lead to their <a href="http://www.actionsquad.org">website</a> and I read it like a book you can't put down. I was excited and inspired to do the same exploring that they had done, in the dark, underground, where you shouldn't be. So I researched as much as I could, I found everything that appears on <a href="../culture"><em>culture</em></a> and more, you'd be suprised at how much you can find when you dig. </p>
+      <h2>Disclaimer (move to front page)</h2>
       <p>yada yada yada</p>
       <h2>Credits</h2>
       <h3>Social Media</h3>
--- culture/index.php
+++ culture/index.php
@@ -8,7 +8,7 @@
     <div class="jumbotron">
       <div class="container">
         <h1 id="tagline">Culture</h1>
-        <p id="subtagline">the arts and other manifestations of human intellectual achievement regarded collectively</p>
+        <p id="subtagline">arts and other manifestations of human intellectual achievement regarded collectively</p>
       </div>
     </div>
     <div class="container">
@@ -17,10 +17,9 @@
         <p>This is a little piece from Action Squad about their ethics and such.</p>
         <h3><a href="http://www.actionsquad.org/Greg-Brick-Subterranean-Twin-Cities.htm">Greg Brick Fiasco</a></h3>
         <p>read about that shit</p>
-        <p><br>comb through the resources below for articles pertaining to ethics</p>
       <h2>Politics</h2>
         <h3>The Value of Trespass</h3>
-        <div class="embed-responsive embed-responsive-16by9"><iframe width="560" height="315" src="https://www.youtube.com/embed/Tvrjt186khA" frameborder="0" allowfullscreen></iframe></div>
+        <div class="embed-responsive embed-responsive-16by9"><iframe width="560" height="315" src="https://www.youtube.com/embed/f__NmDcLMKA" frameborder="0" allowfullscreen></iframe></div>
       <h2>History</h2>
         <a href="http://www.actionsquad.org/ramble01.html"><h3><em>The Past, Present, and Future of Urban Exploration</em></a></h3>
         <p>by Max Action of Action Squad</p>
@@ -28,32 +27,37 @@
       
       <h2>Books</h2>
         <a href="http://www.gregbrick.org/"><h3>Subterannean Twin Cities</a> by Greg Brick</h3>
-        <a href="http://www.randomhouse.de/book/Subterranean-London-Cracking-the-Capital/Bradley-Garrett/e448393.rhd?pub=58500"><h3>Subterranean London</a> by Bradley Garrett
-      <a href="http://www.actionsquad.org/"><h2>Action Squad</h2></a>
-        <p>write up something good for these guys, and mention that some of these links were broken (html vs htm)</p>
-        <h3><a href="http://teapotshappen.com/about/">Teapots Happen blog</a></h3>
-        <p>Max has had some pretty lucid experiences apparently, you should share these memories</p>
-        <h3><a href="http://www.actionsquad.org/tunnelrat.htm">Tunnel Rat Interview</a></h3>
-        <p>an interview of Max Action published August 2002 in Minnesota Monthly Magazine</p>
-        <h3><a href="http://www.actionsquad.org/crawlspace1.html">The Teapot Story (Max Action's crawlspace)</a></h3>
+        <a href="http://www.randomhouse.de/book/Subterranean-London-Cracking-the-Capital/Bradley-Garrett/e448393.rhd?pub=58500"><h3>Subterranean London</h3></a> by Bradley Garrett
+      <h2><a href="http://www.actionsquad.org"><img src="/resources/asheader.gif" class="img-responsive" alt="Action Squad"></a></h2>
+        <p>These guys and gals and Max Action are so great I gave them a spot above the other Minnesota Explorers... maybe they should be equal. A few of the links on ActionSquad.org are still broken (.html vs .htm) too, a couple of those are corrected here.</p>
+        <h3><a href="http://www.actionsquad.org/tunnelrat.htm">Tunnel Rat</a></h3>
+        <p>Minnesota Monthly Magazine interviewed Max Action, published in August 2002. It's a good introduction to Urban Exploration and offers some insight into the mind of Max Action.</p>
+        <h3><a href="http://teapotshappen.com/about/">Teapots Happen</a></h3>
+        <p>Max has had quite the experience thus far with life and I highly recommend reading what he had written about it. <em>Teapots Happen</em> is his "synchronicity" blog about all the magical stuff that has happened since <a href="http://www.actionsquad.org/crawlspace1.html">exploring his crawlspace</a>. I find it very interesting.</p>
+        <a href="http://www.actionsquad.org/compilation.htm"><h3>Compilation CD</h3></a>
+        <p>This is just some songs that Action Squad put together in the spirit of exploring. I think we'll have our own version of this someday.</p>
+        <h3><a href="http://www.quesehrafarm.com/">Que Sehra Farm</a></h3>
+        <p>You might be wondering where Max is now. Well, on his farm in Colorado of course. Check it the hell out.</p>
         <h3>Max Action's Favorite Things</h3>
         <audio controls><source src="../resources/UEFavoriteThings.mp3" type="audio/mpeg"></source></audio>
-        <a href="http://www.actionsquad.org/compilation.htm"><h3>Compilation CD</h3></a>
-        <p>something ACtion Squad put together for exploring</p>
         <h3>George W. Bush Graphic</h3>
         <img src="../resources/UEisNotaCrime.jpg" class="img-responsive">
-        <h3><a href="http://www.quesehrafarm.com/">Que Sehra Farm</a></h3>
-        <p>Max Action's current CSA</p>
       <h2>Minnesota Explorers</h2>
         <a href="http://tcur.org/"><h3>Twin Cities Urban Recon</h3></a>
+        <p>pretty good, last updated June 2014, lots of missions, stories, photos</p>
         <a href="http://urbandecayue.blogspot.com/"><h3>Urban Decay</h3></a>
+        <p>similar to Urban Recon</p>
+        <a href="http://urbanwanderers.blogspot.com/"><h3>Urban Wanderers</h3></a>
+        <p>similar to the last two a</p>
         <a href="http://www.buggrit.com/urbex.html"><h3>Buggrit</h3></a>
         <a href="http://www.aberrant.org/~sand/drain/"><h3>Minneapolis Drain Archive</h3></a>
         <a href="http://www.uer.ca/locations/newlist.asp?n=1&statfilter=160&country=United%20States&province=Minnesota"><h3>Urban Exploration Resource</h3></a>
-        <h3>Giant Asparagus</h3>
+        <a href="http://www.giantasparagus.com"><h3>Giant Asparagus</h3></a>
+        <p>He's done a lot of other stuff and only recently put out anything related to Urban Exploration, but they're pretty good clips so I thought I'd share them.</p>
         <div class="embed-responsive embed-responsive-16by9"><iframe width="560" height="315" src="https://www.youtube.com/embed/IJ2llQD0or8" frameborder="0" allowfullscreen></iframe></div>
         <div class="embed-responsive embed-responsive-16by9"><iframe width="560" height="315" src="https://www.youtube.com/embed/Tvrjt186khA" frameborder="0" allowfullscreen></iframe></div>
       <h2>Wikipedia</h2>
+      <p>There's several articles out there that that can give some background information or further reading on things.</p>
         <a href="https://en.wikipedia.org/wiki/Urban_exploration"><h3>Urban Exploration</h3></a>
         <a href="https://en.wikipedia.org/wiki/Roof_and_tunnel_hacking"><h3>Roof and Tunnel Hacking</h3></a>
         <a href="https://en.wikipedia.org/wiki/Caving"><h3>Caving</h3></a>
@@ -70,6 +74,7 @@
         <a href="https://en.wikipedia.org/wiki/Catacombs"><h3>Catacombs</h3></a>
         <p>add Drain</p>
         <h2>Further Resources</h2>
+        <p>If anything couldn't fit in above, it's down here.</p>
           <h3>Infiltration</h3>
     </div>
     <?php require('../resources/footer.php');?>
