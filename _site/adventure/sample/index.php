596b2f56d79a188e15da4ab6e8b5164e1783fc85
michaelnv710 <home@Michaels-MacBook-Pro.local>
2016-01-25 19:39:27 -0600

    jekyll migration
    replaces php includes and makes building the site easier

---
+++ _site/adventure/sample/index.php
@@ -0,0 +1,24 @@
+<html lang="en">
+  <?php require('../../resources/head.php');?>
+  <title>Sample Mission</title>
+  </head>
+  <body id="missions">
+    <?php require('../../resources/header.php');?>
+    <div class="jumbotron">
+      <div class="container">
+        <h1 id="tagline">PlaceName</h1>
+        <p id="subtagline">a tagline summarizing the place</p>
+      </div>
+    </div>
+    <div class="container">
+      <h2>History</h2>
+      <p>construction, purpose, all that shit, plus how I got to know the place</p>
+      <h3>Map</h3>
+      <p>detailed map but not revealing of location, just relativities</p>
+      <h2>Mission Name</h2>
+      <p>how you got in, with who, what's in there... mix in media</p>
+      <h3></h3>
+    </div>
+    <?php require('../../resources/footer.php');?>
+  </body>
+</html>
